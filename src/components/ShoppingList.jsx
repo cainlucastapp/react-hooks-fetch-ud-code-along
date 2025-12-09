419aa5843ccc6fd451d143fd47f89f5661a120ed
cainlucastapp <cainlucastapp@agentformula.com>
2025-12-09 10:13:02 -0800

    added add and remove from cart

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -20,6 +20,17 @@ function ShoppingList() {
       .catch(error => console.log(error))
   }, []);
 
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map(item => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  }
+
   function handleAddItem(newItem) {
     setItems([...items, newItem]);
   }
@@ -42,8 +53,9 @@ function ShoppingList() {
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+        {/* pass it as a prop to Item */}
+        {itemsToDisplay.map(item => (
+          <Item key={item.id} item={item} onUpdateItem={handleUpdateItem} />
         ))}
       </ul>
     </div>
